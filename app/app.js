96d2486de396704d40e18567ac41c3ed791637f7
fonfonx <xavier.fontaine@polytechnique.org>
2016-11-19 16:43:40 +0100

    :sparkles: Fetch funds values

--- app/app.js
+++ app/app.js
@@ -1,6 +1,68 @@
+const baseURL = 'https://www.afer.asso.fr/amcharts/'
+
+const funds = [
+  {
+    key: 'DIVDUR',
+    name: 'Afer Diversifié Durable',
+  },
+  {
+    key: 'SFER',
+    name: 'Afer Sfer',
+  },
+  {
+    key: 'ACEURO',
+    name: 'Afer Actions Euro',
+  },
+  {
+    key: 'ACMOND',
+    name: 'Afer Actions Monde',
+  }
+]
+
+processCSV = function(data) {
+  arr = data.split(/;|\r/);
+  valueHistory = arr.reduce(function(acc, current, index, arr) {
+    if (index % 2 === 0)
+    {
+      acc.push({
+        date: arr[index],
+        value: parseFloat(arr[index + 1]),
+      })
+      return acc;
+    }
+    else {
+      return acc;
+    }
+  }, [])
+  return valueHistory
+}
+
 var afer = new Vue({
   el: '.afer',
   data: {
-    message: 'Hello Afer!'
-  }
+    message: 'Hello Afer!',
+    funds: funds,
+    values: [],
+  },
+  methods: {
+    getDIVDUR: function() {
+      fundValues = [];
+      for (fund of funds) {
+        const reqURL = baseURL + fund.key + '_data.csv';
+        this.$http.get(reqURL).then((response) => {
+          // success callback
+          const valueHistory = processCSV(response.body);
+          fundValues.push({
+            key: fund.key,
+            history: valueHistory,
+          })
+        }, (response) => {
+          // error callback
+          console.log('Unable to fetch csv');
+        });
+      }
+      this.values = fundValues;
+    }
+  },
+
 })
