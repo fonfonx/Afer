f1c8e768430cb0bda2d7aec5492453461e93c137
fonfonx <xavier.fontaine@polytechnique.org>
2016-12-03 23:59:12 +0100

    :wrench: Add browserify to the project

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,18 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('browserify', function() {
+  return browserify('./app/app.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./www'));
+});
+
 gulp.task('browserSync', function() {
   browserSync({
     host: '127.0.0.1',
@@ -17,7 +29,8 @@ gulp.task('reload', function() {
 })
 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*', ['reload']);
+  gulp.watch('app/**/*', ['browserify']);
+  gulp.watch('www/bundle.js', ['reload'])
   gulp.watch('index.html', ['reload'])
 })
 
